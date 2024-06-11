b523c30c8c80b7e9307bafc78f5b0fe28649c31c
ProjectSoft <projectsoft2009@yandex.ru>
2024-06-11 11:24:03 +0400

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,18 @@
 module.exports = (grunt) => {
 	process.removeAllListeners('warning');
 	require('dotenv').config();
+
+	const running = process.env.DEVELOPMENT == "1" ? false : true,
+		pkg = grunt.file.readJSON('package.json');
+
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+		globalConfig: {},
+		pkg: pkg,
+	});
+	
+	grunt.registerTask('default', [
+	]);
 }
