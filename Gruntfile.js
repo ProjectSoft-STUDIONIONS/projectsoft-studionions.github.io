c7c39b347c350437ca594a9d6f32e439aa3acfd7
ProjectSoft <projectsoft2009@yandex.ru>
2024-06-11 11:53:28 +0400

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,12 @@ module.exports = (grunt) => {
 	require('dotenv').config();
 
 	const running = process.env.DEVELOPMENT == "1" ? false : true,
-		pkg = grunt.file.readJSON('package.json');
+		pkg = grunt.file.readJSON('package.json'),
+		path = require('path'),
+		uniqid = function () {
+			let result = URL.createObjectURL(new Blob([])).slice(-36).replace(/-/g, '');
+			return result;
+		};;
 
 	require('load-grunt-tasks')(grunt);
 	require('time-grunt')(grunt);
@@ -11,8 +16,59 @@ module.exports = (grunt) => {
 	grunt.initConfig({
 		globalConfig: {},
 		pkg: pkg,
+		less: {
+			docs: {
+				options: {
+					compress: false,
+					ieCompat: false,
+					plugins: [
+						
+					]
+				},
+				files: {
+					'test/css/page-main.css': [
+						'src/less/main.less',
+					]
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+				roundingPrecision: -1
+			},
+			docs: {
+				files: {
+					'docs/css/main.css': [
+						//'bower_components/fancybox/dist/jquery.fancybox.css',
+						'test/css/page-main.css'
+					]
+				}
+			}
+		},
+		pug: {
+			page: {
+				options: {
+					pretty: '',// '\t',
+					separator: '',// '\n'
+					data: function(dest, src) {
+						return {
+							"hash": uniqid(),
+							"repo": "projectsoft-studionions.github.io",
+							"userSite": "projectsoft-studionions",
+						}
+					},
+				},
+				files: {
+					"docs/index.html": ["src/pug/index.pug"]
+				},
+			},
+		},
 	});
-	
+
 	grunt.registerTask('default', [
+		"less",
+		"cssmin",
+		"pug"
 	]);
 }
