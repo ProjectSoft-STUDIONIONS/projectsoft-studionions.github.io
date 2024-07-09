cfdc1de73ce48c8bb54acdc80dd7a6ca32375153
ProjectSoft <projectsoft2009@yandex.ru>
2024-07-09 20:46:36 +0400

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('link').forEach((e => {
+/*document.querySelectorAll('link').forEach((e => {
 	const u = new URL(e.href);
 	if(e.rel == "stylesheet" && new URL(u.href, document.baseURI).origin == location.origin){
 		fetch(u.href).then(async function(t) {
@@ -8,4 +8,4 @@ document.querySelectorAll('link').forEach((e => {
 			e.setAttribute("href", s);
 		})
 	}
-}))
+}))*/
